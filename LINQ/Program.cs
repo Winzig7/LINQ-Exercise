d91d40c8ec46041d09118efd8d55e8e6de98e866
tyler ambrose <blitzinc98@yahoo.com>
2023-03-19 20:09:55 -0400

    done

--- LINQ/Program.cs
+++ LINQ/Program.cs
@@ -1,10 +1,18 @@
-﻿namespace LINQ
-{
-    public class Program
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LINQ
+{   public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var videoGames = new List<string> { "Call Of Duty", "Halo", "Destiny" };
+            videoGames.Sort((x, y) => x.Length.CompareTo(y.Length));
+            Console.WriteLine(videoGames);
+            
         }
     }
 }
